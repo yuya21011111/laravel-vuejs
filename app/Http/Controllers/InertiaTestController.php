69f8a0983363a36ddc8cf56a888442bebaf56b0b
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-03 03:48:41 +0900

    Show保存テスト

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InertiaTest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -16,5 +17,14 @@ class InertiaTestController extends Controller
         return Inertia::render('Inertia/Show',[
             'id' => $id
         ]);
+    }
+
+    public function store(Request $request) {
+        $inertiaTest = new InertiaTest;
+        $inertiaTest->title = $request->title;
+        $inertiaTest->content = $request->content;
+        $inertiaTest->save();
+
+        return to_route('inertia.index');
     }
 }
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const newTitle = ref('')
+const newContent = ref('')
 </script>
 
 <template>
     <h1>Inertiaのテストページになります。</h1><br>
     <a class="" href="/">aタグでWelcomeに移動します</a><br>
     <Link href="/">LinkでWelcomeに移動します</Link><br>
-    <Link :href="route('inertia.index')">名前付きルートです</Link>
-    <Link :href="route('inertia.show',{id : 100})">button</Link>
+    <Link :href="route('inertia.index')">名前付きルートです</Link><br>
+    <Link :href="route('inertia.show',{id : 100})">button</Link><br>
+    <input type="text" name="newTitle" v-model="newTitle" />{{ newTitle }}<br>
+    <input type="text" name="newContent" v-model="newContent" />{{ newContent }}<br>
+    <Link as="button" method="post" :href="route('inertia.store')" :data="{
+        title: newTitle,
+        content: newContent
+    }">保存テスト</Link>
+
 </template>
