b587c8d9b7134011e6e34ad4b0dba1ef8c5c620e
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-06 17:13:20 +0900

    defineemit作成

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,10 +2,15 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
+
+const emitTest = e => console.log(e)
 </script>
 <template>
 <GuestLayout>
 <InputLabel value="件名">タイトル</InputLabel>
-<TextInput modelValue="文字を入力してください"></TextInput>
+<TextInput 
+modelValue="文字を入力してください"
+@update:modelValue="emitTest">
+</TextInput>
 </GuestLayout>
 </template>
