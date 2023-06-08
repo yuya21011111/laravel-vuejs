9779b05bd19bc65efa55b6a969b61d0f67763aab
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-09 04:40:14 +0900

    Itemsindex

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -16,6 +16,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カットの詳細',
+                'price' => 6000
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラーの詳細',
+                'price' => 8000
+            ],
+            [
+                'name' => 'パーマ（カット込）',
+                'memo' => 'パーマ、カットの詳細',
+                'price' => 13000
+            ]
+            ]);
     }
 }
