c082d8281e31026d3891e7f9557e838e39fe5af2
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-23 17:47:45 +0900

    購買履歴作成

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -20,7 +20,15 @@ class PurchaseController extends Controller
      */
     public function index()
     {
-        dd(Order::paginate(50));
+        // dd(Order::paginate(50));
+        $orders = Order::groupBy('id')
+        ->selectRaw('id,sum(subtotal) as total,customer_name, status,created_at')
+        ->paginate(50);
+        // dd($orders);
+
+        return Inertia::render('Purchases/Index',[
+            'orders' => $orders
+        ]);
     }
 
     /**
--- resources/js/Pages/Purchases/Index.vue
+++ resources/js/Pages/Purchases/Index.vue
@@ -2,23 +2,30 @@
 import FlashMessage from '@/Components/FlashMessage.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head,Link } from '@inertiajs/vue3';
+import { onMounted, ref } from 'vue';
+import Pagination from '@/Components/Pagination.vue';
+import { Inertia } from '@inertiajs/inertia';
 
 
-defineProps(
-    { items: Array }
+const  props = defineProps(
+    {
+        orders: Object
+    }
 );
+
+onMounted(() => {
+    console.log(props.orders.data)
+})
 
 </script>
 
 <template>
-    
-    <Head title="商品一覧" />
+    <Head title="購買履歴" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">商品一覧</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">購買履歴</h2>
         </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -27,10 +34,12 @@ defineProps(
                             <div class="container px-5 py-8 mx-auto">
                                 <FlashMessage />
                                 <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
-                                  
-                                <Link as="button" :href="route('items.create')"
-                                    class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
-                            </div>
+                                    <div>
+                                        <input type="text" name="search" v-model="search">
+                                        <button class="bg-blue-300 text-white py-2 px-2"
+                                            @click="searchCustomers">検索</button>
+                                    </div>
+                                </div>
                                 <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                     <table class="table-auto w-full text-left whitespace-no-wrap">
                                         <thead>
@@ -40,38 +49,40 @@ defineProps(
                                                     ID</th>
                                                 <th
                                                     class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
-                                                    商品名</th>
+                                                    氏名</th>
                                                 <th
                                                     class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
-                                                    価格</th>
+                                                    合計金額</th>
                                                 <th
                                                     class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                                                     ステータス</th>
-                                                
+                                                <th
+                                                    class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                    購入日</th>
+
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr v-for="item in items" :key="item.id">
+                                            <tr v-for="order in props.orders.data" :key="order.id">
                                                 <td class="border-b-2 border-gray-200 px-4 py-3">
-                                                    <Link class="text-blue-400" :href="route('items.show',{ item: item.id })">{{ item.id }}</Link>
+                                                  {{ order.id }}
                                                 </td>
-                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.name }}</td>
-                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.price }}</td>
-                                                <td class="border-b-2 border-gray-200 px-4 py-3">
-                                                   <span v-if="item.is_selling === 1">販売中</span>
-                                                   <span v-if="item.is_selling === 0">停止中</span>
-                                                </td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ order.customers_name }}</td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ order.total }}</td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ order.status }}</td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ order.created_at }}</td>
                                             </tr>
-                                            
                                         </tbody>
                                     </table>
                                 </div>
-                              
-                        </div>
-                    </section>
+
+                            </div>
+
+                            <Pagination class="mt-4 flex justify-center" :links="props.orders.links" />
+
+                        </section>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</AuthenticatedLayout>
-</template>
+    </AuthenticatedLayout></template>
