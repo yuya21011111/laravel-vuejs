170192e01380f48e9e5fdea3ec0f08239484afa0
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-28 04:23:15 +0900

    Services

---
+++ app/Services/AnalysisService.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Services;
+use Illuminate\Support\Facades\DB;
+
+class AnalysisService {
+    public static function perDay($subQuery){
+
+       $subQuery = $subQuery->where('status',true)
+            ->groupBy('id')
+            ->selectRaw('id, sum(subtotal) as totalPerPurchase,
+            DATE_FORMAT(created_at, "%Y%m%d") as date');
+    
+            $data = DB::table($subQuery)
+            ->groupBy('date')
+            ->selectRaw('date,sum(totalPerPurchase) as total')
+            ->get();
+
+            $labels = $data->pluck('date');
+            $totals = $data->pluck('total');
+
+
+        return [$data,$labels,$totals];
+    }
+}
