7650cdbb034d5a8f1e243ea13e35dc8c411867ed
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-29 02:58:51 +0900

    decile分析テスト

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -13,26 +13,66 @@ class AnalysisController extends Controller
 
         $startDate = '2022-08-01';
         $endDate = '2022-08-31';
-        $period = Order::betweenDate($startDate,$endDate)
-        ->groupBy('id')
-        ->selectRaw('id,sum(subtotal) as total,
-        customer_name,status,created_at')
-        ->orderBy('created_at')
-        ->paginate(50);
-
+       
         $subQuery = Order::betweenDate($startDate,$endDate)
-        ->where('status',true)->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as totalPerPurchase,
-        DATE_FORMAT(created_at, "%Y%m%d") as date');
+        ->groupBy('id')
+        ->selectRaw('id,customer_id,customer_name,sum(subtotal) as totalPerPurchase');
 
-        $data = DB::table($subQuery)
-        ->groupBy('date')
-        ->selectRaw('date,sum(totalPerPurchase) as total')
-        ->get();
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id,customer_name,sum(totalPerPurchase) as total')
+        ->orderBy('total','desc');
 
-        // dd($data);
+        // dd($subQuery);
 
-        // dd($period);
+        DB::statement('set @row_num = 0;');
+          $subQuery = DB::table($subQuery)
+          ->selectRaw(
+            '@row_num:= @row_num+1 as row_num,
+            customer_id,
+            customer_name,
+            total'
+          );
+
+        //   dd($subQuery);
+
+          $count = DB::table($subQuery)->count();
+          $total = DB::table($subQuery)->selectRaw('sum(total) as total')->get();
+          $total = $total[0]->total;
+
+          $decile = ceil($count / 10);
+
+          $bindValues = [];
+          $tempValue = 0;
+
+          for($i = 1; $i <= 10; $i++){
+            array_push($bindValues, 1 + $tempValue);
+            $tempValue += $decile;
+            array_push($bindValues, 1 + $tempValue);
+          }
+
+          dd($count,$decile,$bindValues);
+          
+          DB::statement('set @row_num = 0;');
+          $subQuery = Db::table($subQuery)
+          ->selectRaw("
+          row_num,
+          customer_id,
+          customer_name,
+          total,
+          case
+            when ? <= row_num and row_num < ? then 1
+            when ? <= row_num and row_num < ? then 2
+            when ? <= row_num and row_num < ? then 3
+            when ? <= row_num and row_num < ? then 4
+            when ? <= row_num and row_num < ? then 5
+            when ? <= row_num and row_num < ? then 6
+            when ? <= row_num and row_num < ? then 7
+            when ? <= row_num and row_num < ? then 8
+            when ? <= row_num and row_num < ? then 9
+            when ? <= row_num and row_num < ? then 10
+        end as decile"
+        ,$bindValues);
         return Inertia::render('Analysis');
     }
 }
